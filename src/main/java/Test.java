6d568bfaeedd4827169770de0cf747d6ed45c154
geyangchen.gyc <geyangchen.gyc@alibaba-inc.com>
2022-09-30 16:02:17 +0800

    新增Hello World

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,10 @@
  */
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+        System.out.println("Hello World5");
     }
 }
