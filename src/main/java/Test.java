d334104119dbcaf22a6798ec656efc49a6243aab
geyangchen.gyc <geyangchen.gyc@alibaba-inc.com>
2022-10-08 11:09:27 +0800

    fix 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("Hello World3");
         System.out.println("Hello World4");
         System.out.println("Hello World5");
+        System.out.println("fix1");
     }
 }
