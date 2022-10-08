61f255deba739dcd6b76fc5029144a4660676c61
geyangchen.gyc <geyangchen.gyc@alibaba-inc.com>
2022-10-08 11:10:03 +0800

    fix 2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,5 +12,6 @@ public class Test {
         System.out.println("Hello World4");
         System.out.println("Hello World5");
         System.out.println("fix1");
+        System.out.println("fix2");
     }
 }
